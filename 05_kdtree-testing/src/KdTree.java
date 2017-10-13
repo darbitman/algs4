7c34fcc605bf25f434c02e2ed355c39e90de74ee
Dmitriy <d.arbitman@gmail.com>
2017-10-12 20:50:56 -0700

    added add() to KdTree

--- 05_kdtree-testing/src/KdTree.java
+++ 05_kdtree-testing/src/KdTree.java
@@ -3,21 +3,17 @@ import edu.princeton.cs.algs4.*;
 import edu.princeton.cs.algs4.Point2D;
 
 public class KdTree {
-    Node root;
-    
+    private Node root;
+    private int size;
+    private final int X = 1;
+    private final int Y = 2;
+
     private class Node {
         private Point2D point;
         private int orientation;
         private Node left;
         private Node right;
-        
-        public Node(Point2D p, int orientation, Node left, Node right) {
-            this.point = p;
-            this.orientation = orientation;
-            this.left = left;
-            this.right = right;
-        }
-        
+
         public Node(Point2D p, int orientation) {
             this.point = p;
             this.orientation = orientation;
@@ -27,19 +23,50 @@ public class KdTree {
     }
 
     public KdTree() {
-        root = null;
-        
-        // TODO Auto-generated constructor stub
+        this.root = null;
+        this.size = 0;
     }
+    
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        checkNull(p, "insert() has null argument");
+        add(p, this.root, this.root.orientation);
+    }
+
+    private void add(Point2D p, Node n, int orientation) {
+        if (n == null) {
+            n = new Node(p, orientation);
+            this.size++;
+        }
+        if (p.x() == n.point.x() && p.y() == n.point.y()) {
+            return;
+        }
+        if (orientation == this.X) {
+            if (p.x() < n.point.x()) {
+                add(p, n.left, this.Y);
+            }
+            else {
+                add(p, n.right, this.Y);
+            }
+        }
+        else {
+            if (p.y() < n.point.y()) {
+                add(p, n.left, this.X);
+            }
+            else {
+                add(p, n.right, this.X);
+            }
+        }
+    }
+        
 
     private void checkNull(Object o, String msg) {
         if (o == null) {
             throw new java.lang.IllegalArgumentException(msg);
         }
     }
-    
+
     public static void main(String[] args) {
-        KdTree x = new KdTree();
-        
+
     }
 }
