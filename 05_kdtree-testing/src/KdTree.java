114fca10a9a455bbd86fecda3ac12fea862c9b75
Dmitriy <d.arbitman@gmail.com>
2017-10-14 18:49:07 -0700

    Added contains() and add() methods

--- 05_kdtree-testing/src/KdTree.java
+++ 05_kdtree-testing/src/KdTree.java
@@ -30,18 +30,19 @@ public class KdTree {
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         checkNull(p, "insert() has null argument");
-        add(p, this.root, this.root.orientation);
+        add(p, this.root, this.X);
     }
 
+    // recursive add to Kd tree.
     private void add(Point2D p, Node n, int orientation) {
         if (n == null) {
             n = new Node(p, orientation);
             this.size++;
         }
-        if (p.x() == n.point.x() && p.y() == n.point.y()) {
+        else if (p.x() == n.point.x() && p.y() == n.point.y()) {
             return;
         }
-        if (orientation == this.X) {
+        else if (n.orientation == this.X) {
             if (p.x() < n.point.x()) {
                 add(p, n.left, this.Y);
             }
@@ -49,7 +50,7 @@ public class KdTree {
                 add(p, n.right, this.Y);
             }
         }
-        else {
+        else if (n.orientation == this.Y) {
             if (p.y() < n.point.y()) {
                 add(p, n.left, this.X);
             }
@@ -57,8 +58,42 @@ public class KdTree {
                 add(p, n.right, this.X);
             }
         }
+        else {
+            throw new java.lang.IllegalStateException("recursive add() failed");
+        }
     }
-        
+    
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        checkNull(p, "contains() has null argument");
+        return contains(p, this.root); 
+    }
+    
+    private boolean contains(Point2D p, Node n) {
+        if (n == null) {
+            return false;
+        }
+        else if (p.x() == n.point.x() && p.y() == n.point.y()) {
+            return true;
+        }
+        else if (n.orientation == this.X) {
+            if (p.x() < n.point.x()) {
+                return contains(p, n.left);
+            }
+            else if(p.x() > n.point.x()) {
+                return contains(p, n.right);
+            }
+        }
+        else if (n.orientation == this.Y) {
+            if (p.y() < n.point.y()) {
+                return contains(p, n.left);
+            }
+            else if (p.y() > n.point.y()) {
+                return contains(p, n.right);
+            }
+        }
+        return false;
+    }
 
     private void checkNull(Object o, String msg) {
         if (o == null) {
