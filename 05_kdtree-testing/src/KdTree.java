0270eacd62ec7af8eb3156bd2c87c8cb495e9f5d
dmitriy <d.arbitman@gmail.com>
2017-10-19 20:47:34 -0700

    updated KdTree

--- 05_kdtree-testing/src/KdTree.java
+++ 05_kdtree-testing/src/KdTree.java
@@ -1,4 +1,4 @@
-import java.lang.*;
+import java.util.LinkedList;
 import edu.princeton.cs.algs4.*;
 import edu.princeton.cs.algs4.Point2D;
 
@@ -13,12 +13,14 @@ public class KdTree {
         private int orientation;
         private Node left;
         private Node right;
+        private RectHV rect;
 
-        public Node(Point2D p, int orientation) {
+        public Node(Point2D p, int orientation, RectHV rect) {
             this.point = p;
             this.orientation = orientation;
             this.left = null;
             this.right = null;
+            this.rect = rect;
         }
     }
 
@@ -26,73 +28,118 @@ public class KdTree {
         this.root = null;
         this.size = 0;
     }
-    
+
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         checkNull(p, "insert() has null argument");
-        add(p, this.root, this.X);
+        if (this.root == null) {
+            this.root = new Node(p, this.X, new RectHV(0, 0, 1, 1));
+        } else {
+            Node parent = findParent(p);
+            Point2D parentPoint = parent.point;
+            RectHV parentRect = parent.rect;
+
+            // add new node if point isn't equal
+            if (!p.equals(parent.point)) {
+                if (parent.orientation == this.X) {
+                    if (p.x() <= parentPoint.x()) {
+                        parent.left = new Node(p, this.Y, new RectHV(parentRect.xmin(), parentRect.ymin(), parentPoint.x(), parentRect.ymax()));
+                    }
+                    else {
+                        parent.right = new Node(p, this.Y, new RectHV(parentPoint.x(), parentRect.ymin(), parentRect.xmax(), parentRect.ymax()));
+                    }
+                    else {
+                        if (p.y() <= parentPoint.y()) {
+                            parent.left = new Node(p, this.X, new RectHV(parentRect.xmin(), parentRect.ymin(), parentRect.xmax(), parentPoint.y()));
+                        }
+                        else {
+                            parent.right = new Node(p, this.X, new RectHV(parentRect.xmin(), parentPoint.y(), parentRect.xmax(), parentRect.ymax()));
+                        }
+                    }
+                }
+                this.size++;
+            }
+        }
     }
 
-    // recursive add to Kd tree.
-    private void add(Point2D p, Node n, int orientation) {
-        if (n == null) {
-            n = new Node(p, orientation);
-            this.size++;
-        }
-        else if (p.x() == n.point.x() && p.y() == n.point.y()) {
-            return;
-        }
-        else if (n.orientation == this.X) {
-            if (p.x() < n.point.x()) {
-                add(p, n.left, this.Y);
+    private Node findParent(Point2D p) {
+        checkNull(p, "findParent() has null argument");
+        Node currentNode = root;
+        Node parent = currentNode;
+        while (currentNode != null && !p.equals(currentNode.point)) {
+            parent = currentNode;
+
+            // left-right orientation
+            if (currentNode.orientation == this.X) {
+                if (p.x() <= currentNode.point.x()) {
+                    currentNode = currentNode.left;
+                } else {
+                    currentNode = currentNode.right;
+                }
             }
+
+            // top-bottom orientation
             else {
-                add(p, n.right, this.Y);
+                if (p.y() <= currentNode.point.y()) {
+                    currentNode = currentNode.left;
+                } else {
+                    currentNode = currentNode.right;
+                }
             }
         }
-        else if (n.orientation == this.Y) {
-            if (p.y() < n.point.y()) {
-                add(p, n.left, this.X);
-            }
-            else {
-                add(p, n.right, this.X);
-            }
+
+        // point not found, return parent
+        if (currentNode == null) {
+            return parent;
         }
+        // point found
         else {
-            throw new java.lang.IllegalStateException("recursive add() failed");
+            return currentNode;
         }
     }
-    
+
     // does the set contain point p?
     public boolean contains(Point2D p) {
         checkNull(p, "contains() has null argument");
-        return contains(p, this.root); 
+        return contains(p, this.root);
     }
-    
+
     private boolean contains(Point2D p, Node n) {
         if (n == null) {
             return false;
-        }
-        else if (p.x() == n.point.x() && p.y() == n.point.y()) {
+        } else if (p.x() == n.point.x() && p.y() == n.point.y()) {
             return true;
-        }
-        else if (n.orientation == this.X) {
+        } else if (n.orientation == this.X) {
             if (p.x() < n.point.x()) {
                 return contains(p, n.left);
-            }
-            else if(p.x() > n.point.x()) {
+            } else if (p.x() > n.point.x()) {
                 return contains(p, n.right);
             }
-        }
-        else if (n.orientation == this.Y) {
+        } else if (n.orientation == this.Y) {
             if (p.y() < n.point.y()) {
                 return contains(p, n.left);
-            }
-            else if (p.y() > n.point.y()) {
+            } else if (p.y() > n.point.y()) {
                 return contains(p, n.right);
             }
         }
         return false;
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        checkNull(rect, "range() has null argument");
+        LinkedList<Point2D> pointsInsideRect = new LinkedList<Point2D>();
+        range(root, rect, pointsInsideRect);
+        return pointsInsideRect;
+    }
+
+    private void range(Node n, RectHV rect, LinkedList<Point2D> list) {
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+
+        return null;
     }
 
     private void checkNull(Object o, String msg) {
--- 05_kdtree-testing/src/PointSET.java
+++ 05_kdtree-testing/src/PointSET.java
@@ -9,85 +9,83 @@ import edu.princeton.cs.algs4.StdDraw;
 
 public class PointSET {
     private TreeSet<Point2D> set;
-    
-    
+
     // construct an empty set of points
     public PointSET() {
         set = new TreeSet<Point2D>();
     }
-    
+
     // is the set empty?
     public boolean isEmpty() {
         return set.isEmpty();
     }
-    
+
     // number of points in the set
     public int size() {
         return set.size();
     }
-    
+
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         checkNull(p, "insert() has null argument");
         set.add(p);
     }
-    
+
     // does the set contain point p?
     public boolean contains(Point2D p) {
         checkNull(p, "contains() has null argument");
         return set.contains(p);
     }
-    
+
     // draw all points to standard draw
     public void draw() {
-        for (Point2D q: this.set) {
+        for (Point2D q : this.set) {
             StdDraw.point(q.x(), q.y());
         }
     }
-    
+
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
         checkNull(rect, "range() has null argument");
         LinkedList<Point2D> pointsInsideRect = new LinkedList<Point2D>();
-        for (Point2D q: this.set) {
+        for (Point2D q : this.set) {
             if (rect.contains(q)) {
                 pointsInsideRect.add(q);
             }
         }
         return pointsInsideRect;
     }
-    
+
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
         checkNull(p, "nearest() has null argument");
         Point2D nearestPoint = null;
-        for (Point2D q: this.set) {
+        for (Point2D q : this.set) {
             if (p.distanceTo(q) < p.distanceTo(nearestPoint) || nearestPoint == null) {
                 nearestPoint = q;
             }
         }
         return nearestPoint;
     }
-    
+
     private void checkNull(Object o, String msg) {
         if (o == null) {
             throw new java.lang.IllegalArgumentException(msg);
         }
     }
-    
+
     public static void main(String[] args) {
         PointSET points = new PointSET();
         try {
             File inputFile = new File("C:\\Users\\Dmitriy\\eclipse-workspace\\05_kdtree-testing\\src\\input100K.txt");
             Scanner sc = new Scanner(inputFile);
-            while(sc.hasNext()) {
+            while (sc.hasNext()) {
                 points.insert(new Point2D(Double.parseDouble(sc.next()), Double.parseDouble(sc.next())));
             }
             sc.close();
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
-//        points.draw();
+        // points.draw();
     }
 }
