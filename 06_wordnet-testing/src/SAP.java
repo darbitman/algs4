766f730fac4c2ea6c07ec597856890cd6feda290
Dmitriy <d.arbitman@gmail.com>
2017-11-12 19:55:58 -0800

    updated WordNet and SAP

--- 06_wordnet-testing/src/SAP.java
+++ 06_wordnet-testing/src/SAP.java
@@ -1,16 +1,64 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+import edu.princeton.cs.algs4.Queue;
 
 public class SAP {
+    private Digraph g;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
+        g = G;
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        return 0;
+        if (v < 0 || w < 0 || v > g.V() - 1 || w > g.V() - 1) {
+            throw new java.lang.IndexOutOfBoundsException("length() called with a vertex not in digraph");
+        }
+        return findSAP(v, w, "length");
+    }
+    
+    private int findSAP(int v, int w, String flag) {
+        BreadthFirstDirectedPaths dist2w = new BreadthFirstDirectedPaths(g, w);
+        int graphLength = g.V();
+        int minLen = 1 << 31;
+        int minAncestor = -1;
+        Boolean[] marked = new Boolean[graphLength];
+        int[] distTo = new int[graphLength];
+        for (int i = 0; i < graphLength; i++) {
+            marked[i] = false;
+            distTo[i] = 1 << 31;
+        }
+        Queue<Integer> qv = new Queue<Integer>();
+        qv.enqueue(v);
+        distTo[v] = 0;
+        marked[v] = true;
+        while (!qv.isEmpty()) {
+            int curr = qv.dequeue();
+            
+            // quick test. if distance to ancestor from v is longer than minLen
+            // then no need to find distance from w -> ancestor
+            // because that adds to distTo[curr]
+            if (minLen < distTo[curr]) {
+                break;
+            }
+            
+            if (dist2w.hasPathTo(curr) && (dist2w.distTo(curr) + distTo[curr] < minLen)) {
+                minLen = dist2w.distTo(curr) + distTo[curr];
+                minAncestor = curr;
+            }
+            for (int next : g.adj(curr)) {
+                if (!marked[next]) {
+                    distTo[next] = distTo[curr] + 1;
+                    marked[next] = true;
+                    qv.enqueue(next);
+                }
+            }
+                
+        }
+        return -1;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
--- 06_wordnet-testing/src/WordNet.java
+++ 06_wordnet-testing/src/WordNet.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 public class WordNet{
     private Digraph g;
+    private SAP sap;
     
     // maps IDs -> Nouns
     private ST<Integer, String> synsetIDmap;
@@ -20,35 +21,63 @@ public class WordNet{
         }
         In synsetInput = new In(synsets);
         In hypernymInput = new In(hypernyms);
+        
+        // read in noun synset
+        // generate Noun -> ID map
+        // and      ID -> Noun map
         while(synsetInput.hasNextLine()) {
             String[] tokens = synsetInput.readLine().split(",");
             String[] nouns = tokens[1].split(" ");
             int id = Integer.parseInt(tokens[0]);
             synsetIDmap.put(id, tokens[1]);
             for (String n : nouns) {
-                synsetNounMap.put(n, id);
+                if (!synsetNounMap.contains(n)) {
+                    synsetNounMap.put(n, id);
+                }
             }
         }
+        
+        g = new Digraph(synsetIDmap.size());
+        while (hypernymInput.hasNextLine()) {
+            String[] tokens = synsetInput.readLine().split(",");
+            int tail = Integer.parseInt(tokens[0]);
+            for (int i = 1; i < tokens.length; i++) {
+                g.addEdge(tail, Integer.parseInt(tokens[i]));
+            }
+        }
+        
+        sap = new SAP(g);
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return null;
+        return synsetNounMap;
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-        return false;
+        if (word == null) {
+            throw new java.lang.IllegalArgumentException("word argument to isNoun() is null");
+        }
+        return synsetNounMap.contains(word);
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
-        return 0;
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            throw new java.lang.IllegalArgumentException("a noun argument to distance() is not in WordNet");
+        }
+        int idA = synsetNounMap.get(nounA);
+        int idB = synsetNounMap.get(nounB);
+        return sap.length(idA, idB);
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            throw new java.lang.IllegalArgumentException("a noun argument to distance() is not in WordNet");
+        }
         return "";
     }
 
