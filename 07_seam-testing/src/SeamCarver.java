f2967c8e304ef68e0d52715be08faf961b12b476
dmitriy <d.arbitman@gmail.com>
2017-11-21 21:58:44 -0800

    Working on energy()

--- 07_seam-testing/src/SeamCarver.java
+++ 07_seam-testing/src/SeamCarver.java
@@ -29,19 +29,6 @@ public class SeamCarver {
         if (x < 0 || y < 0 || x > (width() - 1) || y > (height() - 1)) {
             throw new java.lang.IllegalArgumentException("x or y argument to energy() out of bounds");
         }
-        
-        // border pixel energy
-        if (x == 0 || y == 0 || y == (height() - 1) || x == (width() - 1)) {
-            return 1000.0;
-        }
-
-
-        // colors and energies above/below/left/right of the pixel at (x,y)
-        Color aboveC, belowC, leftC, rightC;
-        double aboveE, belowE, leftE, rightE;
-        aboveC = p.get(x, y);
-        
-        
         return 0.0;
     }
     
