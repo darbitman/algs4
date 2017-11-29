1164468e828c32d21ada510a5950b571a45c6c09
dmitriy <d.arbitman@gmail.com>
2017-11-28 17:49:06 -0800

    Modified energy()

--- 07_seam-testing/src/SeamCarver.java
+++ 07_seam-testing/src/SeamCarver.java
@@ -3,25 +3,29 @@ import java.awt.Color;
 
 public class SeamCarver {
     private Picture p;
+    private static final double MARGIN_ENERGY = 1000.0;
+    private int height, width;
     
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
-        p = picture;
+        p = new Picture(picture);
+        this.height = this.p.height();
+        this.width = this.p.width();
     }
     
     // current picture
-    public Picture pictre() {
-        return null;
+    public Picture picture() {
+        return new Picture(this.p);
     }
     
     // width of current picture
     public int width() {
-        return p.width();
+        return this.width;
     }
     
     // height of current picture
     public int height() {
-        return p.height();
+        return this.height;
     }
     
     // energy of pixel at column x and row y
@@ -32,7 +36,7 @@ public class SeamCarver {
         
         // return 1000 for border pixels
         if (x == 0 || y == 0 || x == (width() - 1) || y == (height() - 1)) {
-            return 1000.0;
+            return MARGIN_ENERGY;
         }
         else {
             Color aboveC, belowC, leftC, rightC;
@@ -40,7 +44,13 @@ public class SeamCarver {
             belowC = p.get(x, y + 1);
             leftC = p.get(x - 1, y);
             rightC = p.get(x + 1, y);
+            double deltaSquareX = Math.pow(rightC.getRed() - leftC.getRed(), 2)
+                    + Math.pow(rightC.getGreen() - leftC.getGreen(), 2)
+                    + Math.pow(rightC.getBlue() - leftC.getBlue(), 2);
+            
+            double deltaSquareY = Math.pow(belowC, b)
         }
+        
         return 0.0;
     }
     
