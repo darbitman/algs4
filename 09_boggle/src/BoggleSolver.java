b5784c5ad5afcf872a2942be1c6659d4c8f53305
unknown <d.arbitman@gmail.com>
2017-12-13 20:40:16 -0800

    Worked on scoreOf()

--- 09_boggle/src/BoggleSolver.java
+++ 09_boggle/src/BoggleSolver.java
@@ -13,6 +13,27 @@ public class BoggleSolver {
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
+        int wordLength = word.length();
+        if (wordLength >= 8) {
+            return 11;
+        }
+        if (wordLength == 7) {
+            return 5;
+        }
+        if (wordLength == 6) {
+            return 3;
+        }
+        if (wordLength == 5) {
+            return 2;
+        }
+        if (wordLength >= 3) {
+            return 1;
+        }
         return 0;
     }
+    
+    public static void main(String[] args) {
+        // test code here
+    }
+
 }
