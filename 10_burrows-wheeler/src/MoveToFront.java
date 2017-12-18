aad078f8c646cc1b9d670429b32601f48bd67512
unknown <d.arbitman@gmail.com>
2017-12-17 16:34:43 -0800

    Started burrows-wheeler

---
+++ 10_burrows-wheeler/src/MoveToFront.java
@@ -0,0 +1,25 @@
+//import edu.princeton.cs.algs4.BinaryStdIn;
+//import edu.princeton.cs.algs4.BinaryStdOut;
+
+public class MoveToFront {
+    // apply move-to-front encoding, reading from standard input and writing to standard output
+    public static void encode() {
+    }
+    
+    // apply move-to-front decoding, reading from standard input and writing to standard output
+    public static void decode() {
+    }
+    
+    // Burrows-Wheeler transform
+    // Given a typical English text file, transform it into a text file in which
+    // sequences of the same character occur near each other many times.
+    private static void bwTransform() {
+        // do something
+    }
+    
+    // if args[0] is '-', apply move-to-front encoding
+    // if args[0] is '+', apply move-to-front decoding
+    public static void main(String[] args) {
+        // do something
+    }
+}
