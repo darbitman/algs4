f976c1b5a81b80a01a68ff5415aedd70898d5e01
unknown <d.arbitman@gmail.com>
2017-12-18 21:16:28 -0800

    Finished moveToFront() encode algorithm

--- 10_burrows-wheeler/src/MoveToFront.java
+++ 10_burrows-wheeler/src/MoveToFront.java
@@ -1,10 +1,45 @@
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdOut;
 
 public class MoveToFront {
+    private static final int MAX = 256;
+    
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
+        int[] code = new int[MAX];
+        // initialize ordered sequency of 256 extended ASCII characters
+        // i-th position = i-th extended ASCII
+        for (int i = 0; i < MAX; i++) {
+            code[i] = i;
+        }
+        
+        char readCharBinary;
+        int swapTemp, swapNext;
+        while (true) {
+            readCharBinary = BinaryStdIn.readChar();
+            if (readCharBinary == code[0]) {
+                StdOut.println(readCharBinary);
+                BinaryStdOut.write(0, 8);               
+            }
+            else {
+                swapTemp = code[0];
+                for (int i = 1; i < MAX; i++) {
+                    if (readCharBinary == code[i]) {
+                        code[0] = code[i];
+                        code[i] = swapTemp;
+                        BinaryStdOut.write(i, 8);
+                        break;
+                    }
+                    else {
+                        swapNext = code[i];
+                        code[i] = swapTemp;
+                        swapTemp = swapNext;
+                    }
+                }
+            }
+        }
     }
     
     // apply move-to-front decoding, reading from standard input and writing to standard output
@@ -16,12 +51,6 @@ public class MoveToFront {
     // Given a typical English text file, transform it into a text file in which
     // sequences of the same character occur near each other many times.
     private static void bwTransform() {
-        // do something
-    }
-    
-    // initialize ordered sequency of 256 extended ASCII characters
-    // i-th position = i-th extended ASCII
-    private static void initializeOrderedSequence() {
         // do something
     }
     
