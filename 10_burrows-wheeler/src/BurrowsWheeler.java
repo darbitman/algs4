2e6fb72a7a6c9a4dce0f38f86abef75aec921836
unknown <d.arbitman@gmail.com>
2017-12-25 21:42:25 -0800

    Started transform

---
+++ 10_burrows-wheeler/src/BurrowsWheeler.java
@@ -0,0 +1,17 @@
+
+public class BurrowsWheeler {
+    // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
+    public static void transform() {
+        // do nothing
+    }
+
+    // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
+    public static void inverseTransform() {
+        // do nothing
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
