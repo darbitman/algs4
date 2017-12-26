06590a94dc97bfe1b53cbef4f01efe144bed4066
unknown <d.arbitman@gmail.com>
2017-12-25 17:09:27 -0800

    Added CircularSuffixArray

---
+++ 10_burrows-wheeler/src/CircularSuffixArray.java
@@ -0,0 +1,73 @@
+
+public class CircularSuffixArray {
+    String originalSuffix;
+    int length;
+    int[] index;
+    String catString;
+    CircString[] circStringArray;
+    
+    
+    private class CircString {
+        private int begin;
+        private int index;
+    }
+    
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+        if (s == null) {
+            throw new java.lang.IllegalArgumentException("argument to constructor is null");
+        }
+        originalSuffix = s;
+        length = s.length();
+        catString = s + s.substring(0, length - 1);
+        circStringArray = new CircString[length];
+        for (int i = 0; i < length; i++) {
+            circStringArray[i].begin = i;
+            circStringArray[i].index = i;
+        }
+        
+        sort();
+        for (int i = 0; i < length; i++) {
+            System.out.println(catString.substring(circStringArray[i].begin, circStringArray[i].begin + length));
+        }
+    }
+    
+    private void sort() {
+        int R = 256;
+        CircString[] auxCircStringArray = new CircString[length];
+        for (int d = length - 1; d >= 0; d--) {  // LSD radix sort
+            int[] count = new int[R + 1];
+            for (int i = 0; i < length; i++) {  // update counts for all entries at position d in string array
+                count[catString.substring(circStringArray[i].begin, circStringArray[i].begin + length).charAt(d)]++;
+            }
+            for (int r = 0; r < R; r++) {  // update cumulates
+                count[r + 1] += count[r];
+            }
+            for (int i = 0; i < length; i++) {  // move items into aux array
+                auxCircStringArray[count[catString.substring(circStringArray[i].begin, circStringArray[i].begin + length).charAt(d)]++] = circStringArray[i];
+            }
+            for (int i = 0; i < length; i++) {  // copy from aux array into original array
+                circStringArray[i] = auxCircStringArray[i];
+            }
+        }
+    }
+    
+    // length of s
+    public int lenght() {
+        return this.length;
+    }
+    
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        if (i < 0 || i >= length) {
+            throw new java.lang.IllegalArgumentException("index() argument i is out of bounds");
+        }
+        return circStringArray[i].index;
+        
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        CircularSuffixArray test = new CircularSuffixArray("ABRACADABRA!");
+    }
+}
