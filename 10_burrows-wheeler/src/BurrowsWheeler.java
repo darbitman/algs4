f1e695ea09ee255a5be916a0543672bbbbda064d
unknown <d.arbitman@gmail.com>
2017-12-28 12:21:24 -0800

    Finished BurrowWheeler inverseTransform()

--- 10_burrows-wheeler/src/BurrowsWheeler.java
+++ 10_burrows-wheeler/src/BurrowsWheeler.java
@@ -1,8 +1,11 @@
 import edu.princeton.cs.algs4.BinaryStdOut;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.BinaryStdIn;
 
 public class BurrowsWheeler {
-    private 
+    private static final int R = 256;  // alphabet size
+    private static final boolean DEBUG = true;
+    
     // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
     public static void transform() {
         String s = BinaryStdIn.readString();
@@ -28,25 +31,73 @@ public class BurrowsWheeler {
 
     // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
     public static void inverseTransform() {
-        int first = BinaryStdIn.readInt();
+        int[] count = new int[R + 1];  // used for count/cumulates for decoding
+        Queue<Integer>[] fifoIndices = new Queue[R];
+        for (int r = 0; r < R; r++) {
+            fifoIndices[r] = new Queue<Integer>();
+        }
         StringBuilder sb = new StringBuilder();  // t[]
         char readBinaryChar;
-        while (!BinaryStdIn.isEmpty()) {
-            readBinaryChar = BinaryStdIn.readChar();
-            sb = sb.append(readBinaryChar);
+        int counter = 0;
+        int length;  // length of t[]
+
+
+        if (DEBUG == false) {
+            BinaryStdIn.readInt();
+            while (!BinaryStdIn.isEmpty()) {
+                readBinaryChar = BinaryStdIn.readChar();
+                sb = sb.append(readBinaryChar);
+                count[readBinaryChar + 1]++;  // update counts
+                fifoIndices[readBinaryChar].enqueue(counter++);
+            }
+            length = sb.length();
         }
-        int length = sb.length();               // length of t[]
+        else {
+            sb = sb.append("ARD!RCAAAABB");
+            length = sb.length();
+            for (int i = 0; i < length; i++) {
+                count[sb.charAt(i) + 1]++;   // update counts
+                fifoIndices[sb.charAt(i)].enqueue(i);
+            }
+        }
+
+
+        for (int r = 0; r < R; r++) {  // update cumulates
+            count[r + 1] += count[r];
+        }
+        char[] tAux = new char[length];
+        for (int i = 0; i < length; i++) {  // sorted first column
+            tAux[count[sb.charAt(i)]++] = sb.charAt(i);
+        }
+        
+        // store corresponding index for relative order
+        // i < j --> next[i] < next[j]
+        int[] next = new int[length];
+        for (int i = 0; i < length; i++) {
+            next[i] = fifoIndices[tAux[i]].dequeue();
+        }
+
+        StringBuilder sbDecoded = new StringBuilder();
+        int nextIndex = 0;
+        char currentChar;
+        for (int i = 0; i < length; i++) {
+            nextIndex = next[nextIndex];
+            currentChar = tAux[nextIndex];
+            sbDecoded = sbDecoded.append(currentChar);
+        }
+        BinaryStdOut.write(sbDecoded.toString());
         BinaryStdOut.flush();
     }
     
     // if args[0] is '-', apply Burrows-Wheeler transform
     // if args[0] is '+', apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
-        if (args[0].equals("-")) {
-            BurrowsWheeler.transform();
-        }
-        else {
-            BurrowsWheeler.inverseTransform();
-        }
+        BurrowsWheeler.inverseTransform();
+//        if (args[0].equals("-")) {
+//            BurrowsWheeler.transform();
+//        }
+//        else {
+//            BurrowsWheeler.inverseTransform();
+//        }
     }
 }
